2b4c1530f6e6fbd625482b17ce47f32f6764d4be
amanghuman <58965636+amanghuman@users.noreply.github.com>
2023-02-18 20:57:23 -0500

    Catcher!

---
+++ Catcher!/script.js
@@ -0,0 +1,47 @@
+var catcher = document.getElementById('catcher');
+var arrows = document.getElementsByClassName('arrow');
+var leftPos = 175;
+var score = 0;
+
+function moveLeft() {
+	if (leftPos > 0) {
+		leftPos -= 10;
+		catcher.style.left = leftPos + 'px';
+	}
+}
+
+function moveRight() {
+	if (leftPos < 350) {
+		leftPos += 10;
+		catcher.style.left = leftPos + 'px';
+	}
+}
+
+document.addEventListener('keydown', function(event) {
+	if (event.code === 'ArrowLeft') {
+		moveLeft();
+	} else if (event.code === 'ArrowRight') {
+		moveRight();
+	}
+});
+
+function dropArrow() {
+	var arrow = document.createElement('div');
+	arrow.classList.add('arrow');
+	arrow.style.left = Math.floor(Math.random() * 380) + 'px';
+	var topPos = 0;
+	var interval = setInterval(function() {
+		topPos += 10;
+		arrow.style.top = topPos + 'px';
+		if (topPos > 350) {
+			if (arrow.style.left === catcher.style.left) {
+				score++;
+				document.getElementById('score').innerHTML = 'Score: ' + score;
+			}
+			clearInterval(interval);
+			arrow.parentNode.removeChild(arrow);
+		}
+	}, 50);
+}
+
+setInterval(dropArrow, 2000);
